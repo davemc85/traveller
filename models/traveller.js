d4cc51be173c1fb48ca5a7edac1955cfcafd7301
davemc85 <davemcallister@hotmail.co.uk>
2019-04-24 17:09:06 +0100

    mvp completed

--- models/traveller.js
+++ models/traveller.js
@@ -10,23 +10,35 @@ Traveller.prototype.getJourneyStartLocations = function() {
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  const result = this.journeys.map((journey) => {
+    return journey.endLocation;
+  })
+  return result;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  const result = this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  })
+  return result;
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  const result = this.journeys.filter((journey) => {
+    return journey.distance > minDistance;
+  })
+  return result;
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  const result = this.journeys.reduce((totalDistance, journey) => {
+    return totalDistance + journey.distance;
+  }, 0)
+  return result;
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  const result = this.
 };
 
 
